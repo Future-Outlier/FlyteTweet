b5e23943300098364ef69903fb030d84ab2ade0e
Future Outlier <eric901201@gmai.com>
2023-10-19 11:40:01 +0800

    format and secure
    Signed-off-by: Future Outlier <eric901201@gmai.com>

--- FlyteTweet.py
+++ FlyteTweet.py
@@ -1,10 +1,22 @@
+import os
 from flytekit import ImageSpec, task, workflow
 from flytekitplugins.chatgpt import ChatGPTTask
+import requests
+from bs4 import BeautifulSoup
+import tweepy
+
+# Configure through environment variables
+OPENAI_ORG = os.environ.get("OPENAI_ORG")
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 
 chatgpt_job = ChatGPTTask(
     name="chatgpt",
     config={
-        "openai_organization": "org-NayNG68kGnVXMJ8Ak4PMgQv7",
+        "openai_organization": OPENAI_ORG,
         "chatgpt_conf": {
             "model": "gpt-4",
             "temperature": 0.7,
@@ -13,48 +25,38 @@ chatgpt_job = ChatGPTTask(
 )
 
 @task
-def get_article(
-    url: str = "https://flyte.org/blog/getting-started-with-large-language-models-key-things-to-know",
-) -> str:
-    import requests
-    from bs4 import BeautifulSoup
-
+def get_article(url: str) -> str:
+    # Sanitize and validate the URL if needed
     response = requests.get(url)
+    response.raise_for_status()  # raise an exception for HTTP errors
+
     soup = BeautifulSoup(response.text, "html.parser")
     texts = soup.stripped_strings
     all_text = " ".join(texts)
-    message = f"You are a FlyteTweet Bot.\n\
-                I want to summarize this article and introduce to Flyte Users.\n\
-                You don't need to handle the tweetapi, just focus on the summary.\n\
-                Make the return message in 100 characters, this is a Tweet Message.\n\
-                Make sure the return message starts from 'Let's check the latest blog from Flyte!'\n\
-                {all_text}"
+    message = (f"You are a FlyteTweet Bot. I want to summarize this article and introduce "
+               f"to Flyte Users. You don't need to handle the tweetapi, just focus on the "
+               f"summary. Make the return message in 100 characters, this is a Tweet Message. "
+               f"Make sure the return message starts from 'Let's check the latest blog from Flyte!' "
+               f"{all_text}")
 
     return message
 
-
 @task
 def tweet(text: str):
-    import tweepy
-    print("@@@ text:", text)
     client = tweepy.Client(
-        bearer_token="AAAAAAAAAAAAAAAAAAAAAA1XqgEAAAAA1hfKvJHzlCSuY2dVtcrh11%2BhirE%3D6xAO4XMwAnxUd991uObQ3kNmyPWvgCL8XbsNt5qFpWs8DHzHE5",
-        consumer_key="Pdte2Po2e6XmiWmTPYsmm0s42",
-        consumer_secret="5lniLAidFkcKAmfGrI9br6BLqCnU53XcQYZv7FnMP5rRg2MPyn",
-        access_token="1714653527858159616-TRtxqsqAb0STZaeAO3C6WTzqZ1OkmQ",
-        access_token_secret="pLsEQDOjn9MSXVOSaM3DJ409f28dgDdBJkN1FkpIbp9BJ",
+        bearer_token=BEARER_TOKEN,
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET,
     )
     client.create_tweet(text=text)
 
-
 @workflow
-def wf(
-    url: str = "https://flyte.org/blog/getting-started-with-large-language-models-key-things-to-know",
-):
+def wf(url: str):
     message = get_article(url=url)
     message = chatgpt_job(message=message)
     tweet(text=message)
 
-
 if __name__ == "__main__":
-    wf()
+    wf("https://flyte.org/blog/getting-started-with-large-language-models-key-things-to-know")
